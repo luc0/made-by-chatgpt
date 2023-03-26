1ad59f88a62cb50b424ff3fba99e8c721af70f8d
luc0 <lucianoperezt@gmail.com>
2023-03-25 23:29:25 -0300

    push

--- script.js
+++ script.js
@@ -1,65 +1,83 @@
-const videoContainer = document.createElement("div");
-videoContainer.style.position = "fixed";
-videoContainer.style.top = "0";
-videoContainer.style.left = "0";
-videoContainer.style.width = "100%";
-videoContainer.style.height = "100%";
-videoContainer.style.zIndex = "-1";
-document.body.appendChild(videoContainer);
+const QUOTES_API_URL = 'https://type.fit/api/quotes';
+const PEXELS_API_KEY = 'ddjm8OIXvFDtgsNCCPWeH38zaZdgMymJGqe9rjrXfYfGKrKboUSiok62';
+const VIDEO_WRAPPER_CLASS = 'video-wrapper';
+const QUOTE_WRAPPER_CLASS = 'quote-wrapper';
+const SPINNER_CLASS = 'spinner';
 
-const spinner = document.createElement("div");
-spinner.className = "spinner";
-spinner.style.position = "absolute";
-spinner.style.top = "50%";
-spinner.style.left = "50%";
-spinner.style.transform = "translate(-50%, -50%)";
-spinner.innerHTML = "Loading...";
-videoContainer.appendChild(spinner);
+let videoPlayer;
+let quoteData;
 
-fetch("https://type.fit/api/quotes")
-  .then((response) => response.json())
-  .then((data) => {
-    const quote = data[Math.floor(Math.random() * data.length)];
-    const keyword = quote.text
-      .replace(/[^\w\s]/gi, "")
-      .split(" ")
-      .sort((a, b) => b.length - a.length)[0];
+async function fetchQuotes() {
+  const response = await fetch(QUOTES_API_URL);
+  const data = await response.json();
+  quoteData = data[Math.floor(Math.random() * data.length)];
+  renderQuote();
+  renderVideo();
+}
 
-    fetch(
-      `https://api.pexels.com/videos/search?query=${keyword}&per_page=1&page=1`,
-      { headers: { Authorization: "ddjm8OIXvFDtgsNCCPWeH38zaZdgMymJGqe9rjrXfYfGKrKboUSiok62" } }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const video = document.createElement("video");
-        video.style.width = "100%";
-        video.style.height = "100%";
-        video.style.objectFit = "cover";
-        video.setAttribute("autoplay", "");
-        video.setAttribute("loop", "");
-        video.setAttribute("muted", "");
-        videoContainer.appendChild(video);
+async function fetchVideo(word) {
+  const url = `https://api.pexels.com/videos/search?query=${word}&per_page=10`;
+  const response = await fetch(url, {
+    headers: {
+      'Authorization': PEXELS_API_KEY
+    }
+  });
+  const data = await response.json();
+  if (data.videos.length > 0) {
+    const video = data.videos[Math.floor(Math.random() * data.videos.length)];
+    videoPlayer.src = video.video_files[0].link;
+  } else {
+    renderVideo();
+  }
+}
 
-        const source = document.createElement("source");
-        source.setAttribute("src", data.videos[0].video_files[0].link);
-        source.setAttribute("type", "video/mp4");
-        video.appendChild(source);
+function renderQuote() {
+  const quoteWrapper = document.createElement('div');
+  quoteWrapper.classList.add(QUOTE_WRAPPER_CLASS);
 
-        spinner.remove();
+  const quote = document.createElement('h1');
+  quote.innerText = quoteData.text;
+  quoteWrapper.appendChild(quote);
 
-        const quoteContainer = document.createElement("div");
-        quoteContainer.style.position = "absolute";
-        quoteContainer.style.top = "50%";
-        quoteContainer.style.left = "50%";
-        quoteContainer.style.transform = "translate(-50%, -50%)";
-        quoteContainer.style.textAlign = "center";
-        quoteContainer.style.color = "#fff";
-        quoteContainer.style.fontFamily = "sans-serif";
-        quoteContainer.style.fontSize = "3em";
-        quoteContainer.style.lineHeight = "1.2em";
-        quoteContainer.innerHTML = `${quote.text}<br>- ${quote.author}`;
-        videoContainer.appendChild(quoteContainer);
-      })
-      .catch((error) => console.log(error));
-  })
-  .catch((error) => console.log(error));
+  const author = document.createElement('h2');
+  author.innerText = quoteData.author;
+  quoteWrapper.appendChild(author);
+
+  document.body.appendChild(quoteWrapper);
+}
+
+function renderVideo() {
+  const videoWrapper = document.createElement('div');
+  videoWrapper.classList.add(VIDEO_WRAPPER_CLASS);
+
+  const spinner = document.createElement('div');
+  spinner.classList.add(SPINNER_CLASS);
+  videoWrapper.appendChild(spinner);
+
+  const video = document.createElement('video');
+  video.autoplay = true;
+  video.loop = true;
+  video.muted = true;
+  videoPlayer = video;
+  videoWrapper.appendChild(video);
+
+  document.body.appendChild(videoWrapper);
+
+  const word = getLongestWord(quoteData.text);
+  fetchVideo(word);
+}
+
+function getLongestWord(text) {
+  const words = text.replace(/[^\w\s]/gi, '').split(' ');
+  const longestWord = words.reduce((a, b) => a.length >= b.length ? a : b, '');
+  return longestWord;
+}
+
+fetchQuotes();
+
+setTimeout(() => {
+  if (!videoPlayer.src) {
+    const word = getLongestWord(quoteData.text);
+    fetchVideo(word);
+  }
+}, 2000);
