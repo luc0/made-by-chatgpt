c447049f70cedd18594659ba8e56686044d5e3e3
luc0 <lucianoperezt@gmail.com>
2023-03-25 22:03:25 -0300

    push

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
-// Creamos un nuevo botón y lo agregamos al cuerpo del documento
-var boton = document.createElement("button");
-boton.innerHTML = "Cambiar Fondo";
-document.body.appendChild(boton);
+// Array con las frases y autores
+var frases = [
+  { texto: "La creatividad es la inteligencia divirtiéndose", autor: "Albert Einstein" },
+  { texto: "No hay viento favorable para quien no sabe a dónde va", autor: "Séneca" },
+  { texto: "El éxito es la suma de pequeños esfuerzos repetidos día tras día", autor: "Robert Collier" },
+  { texto: "El que quiere hacer algo encuentra un medio, el que no quiere hacer nada encuentra una excusa", autor: "Proverbio Árabe" },
+  { texto: "El miedo es una ilusión", autor: "Michael Jordan" },
+  { texto: "Nunca permitas que la educación te impida aprender algo", autor: "Mark Twain" }
+];
 
-// Función que cambia el color de fondo al azar
-function cambiarFondo() {
-  var colores = ['#FF5733', '#C70039', '#900C3F', '#581845', '#4B0082', '#00FF7F', '#FFFF00'];
-  var indice = Math.floor(Math.random() * colores.length);
-  document.body.style.backgroundColor = colores[indice];
+// Array con los gifs y videos
+var multimedia = [
+  { tipo: "gif", url: "https://media.giphy.com/media/l46Ct9mZ1cpolylp6/giphy.gif" },
+  { tipo: "video", url: "https://www.w3schools.com/html/mov_bbb.mp4" },
+  { tipo: "gif", url: "https://media.giphy.com/media/VeE6XlUAVxNiU/giphy.gif" },
+  { tipo: "video", url: "https://www.w3schools.com/html/movie.mp4" }
+];
+
+// Función que elige un elemento aleatorio de un array
+function aleatorio(array) {
+  var indice = Math.floor(Math.random() * array.length);
+  return array[indice];
 }
 
-// Agregamos un event listener al botón para llamar a la función cambiarFondo() cuando se haga clic en él
-boton.addEventListener("click", cambiarFondo);
+// Función que cambia el fondo a un gif o video aleatorio, y muestra una frase en el centro
+function cambiarFondo() {
+  // Elegir un elemento aleatorio del array de multimedia
+  var elementoMultimedia = aleatorio(multimedia);
+
+  // Crear un elemento HTML para el gif o video y agregarlo al cuerpo de la página
+  if (elementoMultimedia.tipo === "gif") {
+    var multimediaHTML = document.createElement("img");
+    multimediaHTML.src = elementoMultimedia.url;
+  } else if (elementoMultimedia.tipo === "video") {
+    var multimediaHTML = document.createElement("video");
+    multimediaHTML.src = elementoMultimedia.url;
+    multimediaHTML.autoplay = true;
+    multimediaHTML.loop = true;
+    multimediaHTML.muted = true;
+  }
+
+  multimediaHTML.style.position = "absolute";
+  multimediaHTML.style.width = "100%";
+  multimediaHTML.style.height = "100%";
+  multimediaHTML.style.top = "0";
+  multimediaHTML.style.left = "0";
+  document.body.appendChild(multimediaHTML);
+
+  // Elegir una frase aleatoria del array de frases
+  var frase = aleatorio(frases);
+
+  // Crear un elemento HTML para la frase y agregarlo al cuerpo de la página
+  var fraseHTML = document.createElement("div");
+  fraseHTML.innerHTML = "<p>" + frase.texto + "</p><p><em>" + frase.autor + "</em></p>";
+  fraseHTML.style.position = "absolute";
+  fraseHTML.style.top = "50%";
+  fraseHTML.style.left = "50%";
+  fraseHTML.style.transform = "translate(-50%, -50%)";
+  fraseHTML.style.textAlign = "center
