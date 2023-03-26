abd468103ec95a321f9e7927177cb655ab0c27d2
luc0 <lucianoperezt@gmail.com>
2023-03-25 22:54:24 -0300

    push

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
-const API_KEY = "ddjm8OIXvFDtgsNCCPWeH38zaZdgMymJGqe9rjrXfYfGKrKboUSiok62";
 const videoContainer = document.createElement("div");
-const textContainer = document.createElement("div");
+videoContainer.style.position = "fixed";
+videoContainer.style.top = "0";
+videoContainer.style.left = "0";
+videoContainer.style.width = "100%";
+videoContainer.style.height = "100%";
+videoContainer.style.zIndex = "-1";
+document.body.appendChild(videoContainer);
+
 const spinner = document.createElement("div");
 spinner.className = "spinner";
+spinner.style.position = "absolute";
+spinner.style.top = "50%";
+spinner.style.left = "50%";
+spinner.style.transform = "translate(-50%, -50%)";
+spinner.innerHTML = "Loading...";
+videoContainer.appendChild(spinner);
 
-const getVideoUrl = async () => {
-  try {
-    const response = await fetch(
-      `https://api.pexels.com/videos/search?query=nature&per_page=80`,
-      { headers: { Authorization: API_KEY } }
-    );
-    const { videos } = await response.json();
-    const video = videos[Math.floor(Math.random() * videos.length)];
-    return video.video_files.find((file) => file.quality === "hd").link;
-  } catch (err) {
-    console.error(err);
-  }
-};
+fetch("https://type.fit/api/quotes")
+  .then((response) => response.json())
+  .then((data) => {
+    const quote = data[Math.floor(Math.random() * data.length)];
+    const keyword = quote.text
+      .replace(/[^\w\s]/gi, "")
+      .split(" ")
+      .sort((a, b) => b.length - a.length)[0];
 
-const getQuote = async () => {
-  try {
-    const response = await fetch(
-      "https://type.fit/api/quotes"
-    );
-    const quotes = await response.json();
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    return quote;
-  } catch (err) {
-    console.error(err);
-  }
-};
+    fetch(
+      `https://api.pexels.com/videos/search?query=${keyword}&per_page=1&page=1`,
+      { headers: { Authorization: "ddjm8OIXvFDtgsNCCPWeH38zaZdgMymJGqe9rjrXfYfGKrKboUSiok62" } }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const video = document.createElement("video");
+        video.style.width = "100%";
+        video.style.height = "100%";
+        video.style.objectFit = "cover";
+        video.setAttribute("autoplay", "");
+        video.setAttribute("loop", "");
+        video.setAttribute("muted", "");
+        videoContainer.appendChild(video);
 
-const setVideoBackground = async () => {
-  spinner.style.display = "block";
-  const videoUrl = await getVideoUrl();
-  videoContainer.innerHTML = `<video autoplay loop muted playsinline style="object-fit: cover; position: fixed; top: 0; left: 0; height: 100%; width: 100%; z-index: -1;">
-  <source src="${videoUrl}" type="video/mp4">
-  Your browser does not support the video tag.
-</video>`;
-  const quote = await getQuote();
-  textContainer.innerHTML = `<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; text-align: center; font-size: 5vw; font-weight: bold; line-height: 1.5; text-shadow: 2px 2px black;">
-  ${quote.text}
-  <br><br>
-  <span style="font-size: 3vw; font-weight: normal;">${quote.author}</span>
-</div>`;
-  spinner.style.display = "none";
-};
+        const source = document.createElement("source");
+        source.setAttribute("src", data.videos[0].video_files[0].link);
+        source.setAttribute("type", "video/mp4");
+        video.appendChild(source);
 
-setVideoBackground();
-document.body.appendChild(videoContainer);
-document.body.appendChild(textContainer);
-document.body.appendChild(spinner);
+        spinner.remove();
+
+        const quoteContainer = document.createElement("div");
+        quoteContainer.style.position = "absolute";
+        quoteContainer.style.top = "50%";
+        quoteContainer.style.left = "50%";
+        quoteContainer.style.transform = "translate(-50%, -50%)";
+        quoteContainer.style.textAlign = "center";
+        quoteContainer.style.color = "#fff";
+        quoteContainer.style.fontFamily = "sans-serif";
+        quoteContainer.style.fontSize = "3em";
+        quoteContainer.style.lineHeight = "1.2em";
+        quoteContainer.innerHTML = `${quote.text}<br>- ${quote.author}`;
+        videoContainer.appendChild(quoteContainer);
+      })
+      .catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
